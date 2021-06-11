14ed6a70137f57e34de77950b85750d14d7d8af8
JunTeng1113 <junteng.1113@gmail.com>
2021-06-11 20:18:25 +0800

    2021/06/11

---
+++ 小雞上工/利用與更新檔案資料/main.py
@@ -0,0 +1,38 @@
+
+data = dict()
+
+# 以 utf-8 編碼讀取 data.txt 檔案
+file = open("data.txt", "r", encoding="utf8")
+
+for line in file:
+    tmp = line.split()
+    data.setdefault(tmp[0], tmp[1]) #將data設定為字典, 以姓名查找生日
+
+file.close()
+
+while True:
+    name = input("請輸入要查詢的姓名:")
+
+    if name in data.keys():
+        birth = data[name]
+        print(f"{birth} 是 {name} 的生日")
+
+    else:
+        print(f"抱歉 查無 {name} 的資料 請輸入 {name} 的生日資料, 我會更新資料庫")
+        birth = input(f"{name} 的生日是幾月幾日?\n")
+
+        # 以 append 附加的方式寫入資料
+        file = open("data.txt", "a", encoding="utf8") 
+        file.write(f"\n{name} {birth}")
+        file.close()
+        print("謝謝 生日資料庫已更新, 歡迎查詢")
+        
+        # 重新讀取資料
+        file = open("data.txt", "r", encoding="utf8")
+
+        for line in file:
+            tmp = line.split()
+            data.setdefault(tmp[0], tmp[1])
+
+        file.close()
+
